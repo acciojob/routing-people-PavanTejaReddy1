a00bb3e2ce878414e9357fb501ef219fb2454241
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-24 00:26:14 +0530

    Fetch and display user list from API

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,1 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function UsersList() {
+    const [users, setUsers] = useState([]);
+
+    async function usersList() {
+        const usersRes = await fetch("https://jsonplaceholder.typicode.com/users")
+        const userData = await usersRes.json();
+        setUsers(userData);
+    }
+
+    useEffect(()=>{
+            usersList();
+    }, [])
+
+  return (
+    <div>
+        <h1>User List</h1>
+        <ul>
+            {users.map((user)=>{
+                return <li><Link to={`/users/${user.id}`}>{user.name}</Link></li>
+            })}
+        </ul>
+    </div>
+  )
+}
+
+export default UsersList
 
