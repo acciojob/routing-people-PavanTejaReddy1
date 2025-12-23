5d243d06fb5387022a2ca9c62ed4812ed638c21f
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-24 00:25:41 +0530

    Update UserDetails.jsx

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,1 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
+function UserDetails() {
+    const { id } = useParams();
+    const [user, setuser] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    async function getUserDetails(id) {
+        setLoading(true);
+        const userRes  = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const userData = await userRes.json();
+        setuser(userData);
+        setLoading(false);
+    }
+  
+    useEffect(()=>{
+        getUserDetails(id);
+    }, [id])
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+  return (
+    <div>
+        <h1>User Details</h1>
+        <p>Name: {user.name}</p>
+        <p>UserName: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Website: {user.website}</p>
+    </div>
+  )
+}
+
+export default UserDetails
