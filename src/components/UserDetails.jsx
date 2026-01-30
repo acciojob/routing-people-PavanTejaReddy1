b6aff6d0fcc4c3b8fa0bed2ef9bfc7e9a0b311ec
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2026-01-30 18:22:22 +0530

    Update UserDetails.jsx

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,37 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function UserDetails() {
-    const { id } = useParams();
-    const [user, setuser] = useState({});
-    const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    async function getUserDetails(id) {
-        setLoading(true);
-        const userRes  = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-        const userData = await userRes.json();
-        setuser(userData);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
         setLoading(false);
-    }
-  
-    useEffect(()=>{
-        getUserDetails(id);
-    }, [id])
+      });
+  }, [id]);
 
-    if(loading) {
-        return <div>Loading...</div>
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-        <h1>User Details</h1>
-        <p>Name: {user.name}</p>
-        <p>UserName: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Phone: {user.phone}</p>
-        <p>Website: {user.website}</p>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
     </div>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
